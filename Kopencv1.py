dd88a3fcb4a31506f7e0d86fbbf28f012ebae675
SSKudos <74167601+SSKudos@users.noreply.github.com>
2020-11-11 00:24:50 +0100

    Create Kopencv1.py

---
+++ Kopencv1.py
@@ -0,0 +1,17 @@
+from __future__ import print_function     
+import numpy
+import cv2   
+
+# image path   
+image_path = (r'C:\Users\CHIJINDU\Desktop\SAMPLE.jpg')     
+# Read or load image from its path     
+image = cv2.imread(image_path)     
+# image is a NumPy array     
+print("Dimensions of the image: ", image.ndim)   
+print("Image height: ", format(image.shape[0]))    
+print("Image width: ", format(image.shape[1]))   
+print("Image channels: ", format(image.shape[2]))
+print("Size of the image array: ", image.size) 
+    # Display the image and wait until a key is pressed    
+cv2.imshow("My Image", image)    
+cv2.waitKey(0)
