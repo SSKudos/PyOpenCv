e4ed73effcfd6fc87b18af32ccabe087091d19ee
SSKudos <74167601+SSKudos@users.noreply.github.com>
2020-11-11 00:26:53 +0100

    Create KOpen1.py

---
+++ KOpen1.py
@@ -0,0 +1,19 @@
+from __future__ import print_function     
+import cv2     
+import numpy as np 
+
+# Load image     
+imagePath = (r'C:\Users\CHIJINDU\Desktop\SAMPLE.jpg')     
+image = cv2.imread(imagePath)     
+(h,w) = image.shape[:2]
+
+#Define translation matrix    
+center = (h//2, w//2)    
+angle = -45    
+scale = 1.0    
+rotationMatrix = cv2.getRotationMatrix2D(center, angle, scale) 
+
+# Rotate the image     
+rotatedImage = cv2.warpAffine(image, rotationMatrix, (image.shape[1], image.shape[0]))    
+cv2.imshow("Rotated image", rotatedImage)    
+cv2.waitKey(0)
